3d47a9518ac0c227a72293be573aea08275a0ef5
Nguyen Tran Thien An <153171508+tawannt@users.noreply.github.com>
2026-03-11 08:52:59 +0700

    Add solution for leaky-relu

---
+++ leaky-relu/leaky-relu.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def leaky_relu(x, alpha=0.01):
+    """
+    Vectorized Leaky ReLU implementation.
+    """
+    # Write code here
+    if not isinstance(x, np.ndarray):
+        x = np.array(x, dtype=float)
+
+    mask = x < 0
+    x[mask] = alpha * x[mask]
+
+    return x
+    
