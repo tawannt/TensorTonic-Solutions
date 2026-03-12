90f6fdd0b7ca9b943237b4bbd9eaf5f605be6006
Nguyen Tran Thien An <153171508+tawannt@users.noreply.github.com>
2026-03-12 22:48:04 +0700

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -6,11 +6,24 @@ def positional_encoding(seq_len, d_model, base=10000.0):
     Odd d_model -> last column is sin.
     """
     # Write code here
-    positional_encoding_embedding = np.zeros((seq_len, d_model), dtype=float)
-    for pos, pos_val in enumerate(positional_encoding_embedding):
-        for i in range(d_model):
-            if i % 2 == 0:
-                pos_val[i] = np.sin(pos / ((base) **(i / d_model)))
-            else:
-                pos_val[i] = np.cos(pos / ((base) **((i - 1) / d_model)))
-    return positional_encoding_embedding
+    # equal to np.arrange(seq_len).reshape(seq_len, 1)
+    pos = np.arange(seq_len)[:, None] 
+    i = np.arange(d_model)[None, :]
+
+    angle_rate = 1 / (base ** (2 * (i // 2) / d_model))
+    angle = pos * angle_rate
+
+    pe = np.zeros((seq_len, d_model))
+    pe[:, 0::2] = np.sin(angle[:, 0::2])
+    pe[:, 1::2] = np.cos(angle[:, 1::2])
+
+    return pe
+    
+    # positional_encoding_embedding = np.zeros((seq_len, d_model), dtype=float)
+    # for pos, pos_val in enumerate(positional_encoding_embedding):
+    #     for i in range(d_model):
+    #         if i % 2 == 0:
+    #             pos_val[i] = np.sin(pos / ((base) **(i / d_model)))
+    #         else:
+    #             pos_val[i] = np.cos(pos / ((base) **((i - 1) / d_model)))
+    # return positional_encoding_embedding
