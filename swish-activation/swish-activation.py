87d3b1b2f079864f965ceed453caa8f8ad981c98
Nguyen Tran Thien An <153171508+tawannt@users.noreply.github.com>
2026-03-13 09:55:07 +0700

    Add solution for swish-activation

---
+++ swish-activation/swish-activation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def swish(x):
+    """
+    Implement Swish activation function.
+    """
+    # Write code here
+    x = np.array(x, dtype=float)
+    sigmoid = 1 / (1+ np.exp(-x))
+    return x * sigmoid
