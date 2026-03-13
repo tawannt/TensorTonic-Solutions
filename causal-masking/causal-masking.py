01750631ff68005222cbed856bf556c800a8e9ad
Nguyen Tran Thien An <153171508+tawannt@users.noreply.github.com>
2026-03-13 10:28:38 +0700

    Add solution for causal-masking

---
+++ causal-masking/causal-masking.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def apply_causal_mask(scores, mask_value=-1e9):
+    """
+    scores: np.ndarray with shape (..., T, T)
+    mask_value: float used to mask future positions (e.g., -1e9)
+    Return: masked scores (same shape, dtype=float)
+    """
+    # Write code here
+    T = scores.shape[-1]
+    masked_scores = scores.copy()
+    # if not dtype=bool, default type = float
+    # => causing exception: arrays used as indices must be of integer (or boolean) type
+    mask = np.triu(np.ones((T, T), dtype=bool), k=1) 
+    print(mask)
+
+    masked_scores[..., mask] = mask_value
+    return masked_scores
+        
+
+    
