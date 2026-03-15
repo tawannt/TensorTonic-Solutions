015bf08f6e0766ec4151aa88e367170fc70eedc6
Nguyen Tran Thien An <153171508+tawannt@users.noreply.github.com>
2026-03-15 17:20:42 +0700

    Update solution for rnn-cell

--- rnn/rnn-cell/rnn-cell.py
+++ rnn/rnn-cell/rnn-cell.py
@@ -6,5 +6,7 @@ def rnn_cell(x_t: np.ndarray, h_prev: np.ndarray,
     Single RNN cell forward pass.
     """
     # YOUR CODE HERE
-    h_t = np.tanh(W_hh @ h_prev.T + W_xh @ x_t.T + b_h.reshape(-1, 1))
-    return h_t.T 
+    # h_t = np.tanh(W_hh @ h_prev.T + W_xh @ x_t.T + b_h.reshape(-1, 1))
+    # return h_t.T 
+    h_t = np.tanh(h_prev @ W_hh.T +  x_t @ W_xh.T + b_h)
+    return h_t
