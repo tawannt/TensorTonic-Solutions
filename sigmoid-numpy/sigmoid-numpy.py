35bf27cb39abc26ebaf3d2a5c4d1b51eb8881995
Nguyen Tran Thien An <153171508+tawannt@users.noreply.github.com>
2026-04-07 00:17:42 +0700

    Update solution for sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -4,6 +4,5 @@ def sigmoid(x):
     """
     Vectorized sigmoid function.
     """
-    if isinstance(x, list):
-        x = np.array(x)
-    return 1 / (1 + np.exp(-x))
+    # Write code here
+    return 1 / (1 + np.exp(-np.array(x)))
